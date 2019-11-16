ad5a66833f3e682e50a2413f7e6c1ec8a02647a3
Neil Flatley <n.flatley@zengenti.com>
2019-11-16 04:41:11 +0000

    Move contensis urls to util sub package

--- webpack/define-config.js
+++ webpack/define-config.js
@@ -1,4 +1,6 @@
 const packagejson = require('../package.json');
+const { urls } = require('zengenti-isomorphic-base/util');
+
 require('custom-env').env(process.env.env || process.env.npm_config_env);
 
 const { PUBLIC_URL, ALIAS, INTERNAL_VIP, ACCESS_TOKEN, PROJECT } = process.env;
@@ -11,11 +13,11 @@ const PROJECTS = env => [
   // You only need to include extra projects in this array
   // if you wish to switch between projects at runtime,
   // or fetch data from another project
+  // {
+  //   id: 'mock',
+  //   publicUri: 'mock.ludlow.ac.uk',
+  // },
   // ˅˅˅˅˅˅˅˅
-  {
-    id: 'mock',
-    publicUri: 'mock.ludlow.ac.uk',
-  },
   // ˄˄˄˄˄˄˄˄
 ];
 
@@ -30,39 +32,27 @@ const ALLOWED_GROUPS = {
   // 1 is Everyone, -1 is false (no security)
   website: [-1],
   mock: [-1],
-  intranet: [1],
 };
 
 // --------------------
 // End of configuration
 // --------------------
 
-const url = () => {
-  const alias = ALIAS;
-  const project = PROJECT;
-  const projectAndAlias =
-    project && project != 'website' ? `${project}-${alias}` : alias;
-  return {
-    cms: `https://cms-${alias}.cloud.contensis.com`,
-    previewWeb: `https://preview-${projectAndAlias}.cloud.contensis.com`,
-    liveWeb: `https://live-${projectAndAlias}.cloud.contensis.com`,
-    iisWeb: `https://iis-live-${projectAndAlias}.cloud.contensis.com`,
-  };
-};
+const url = urls(ALIAS, PROJECT);
 
 const SERVERS = {
   alias: ALIAS,
   internalVip: INTERNAL_VIP,
-  cms: url().cms,
-  web: url().liveWeb,
-  iis: url().iisWeb,
+  cms: url.cms,
+  web: url.liveWeb,
+  iis: url.iisWeb,
 };
 
 const DELIVERY_API_CONFIG = {
-  rootUrl: url().cms,
+  rootUrl: url.cms,
   accessToken: ACCESS_TOKEN,
   projectId: PROJECTS(process.env)[0].id,
-  livePublishingRootUrl: url().previewWeb,
+  livePublishingRootUrl: url.previewWeb,
 };
 
 const development = {
