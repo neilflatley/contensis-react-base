898a276ee1eea56039449191d7b21b2952b196fd
Neil Flatley <n.flatley@zengenti.com>
2020-03-02 18:52:06 +0000

    fix: bump iso-base commit feat: extra navigation utils

--- src/app/core/util/navigation.js
+++ src/app/core/util/navigation.js
@@ -13,3 +13,11 @@ export const buildUrl = (route, params) => {
   const path = qs ? `${route}?${qs}` : route;
   return path;
 };
+
+export const clientHostname = () =>
+  `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+
+export const addHostname =
+  typeof window == 'undefined' || window.location.host == 'localhost:3000'
+    ? `https://${PUBLIC_URI /* global PUBLIC_URI */}`
+    : clientHostname();
